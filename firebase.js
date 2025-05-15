b74c49b9bd744634309e230b28104ed3178eb28f
Munim-Ahmad <munimahmad2@gmail.com>
2025-05-15 23:41:13 +0500

    refactor: update Firebase imports to use module syntax

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js';
-import { getAnalytics } from 'https://www.gstatic.com/firebasejs/11.7.3/firebase-analytics.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/11.7.3/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/11.7.3/firebase-firestore.js';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCvQyAG7LgzNdY1usEHmotUqXH44eYpHjM',
